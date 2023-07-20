354137827f9d33aac40cf6a6fb023ddcadbe7260
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-07-21 01:56:58 +0200

    progressing

--- .init.el
+++ .init.el
@@ -9,3 +9,9 @@
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode 1)
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
